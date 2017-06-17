474006697bb2f65702ba6eb94d19d6bc1e6dde4d
Daniil Belyakov <dnl.blkv@gmail.com>
2017-06-17 14:29:50 +0200

    use internal class variable to the full extent for loading ALCDEF to the associative array

--- src/AlcdefItem.php
+++ src/AlcdefItem.php
@@ -13,9 +13,8 @@ class AlcdefItem
     const NEW_LINE_UNIX = "\n";
 
     /**
-     * Delimiters to split alcdef into meaningful chunks of data.
+     * Delimiters to split ALCDEF document into lines.
      */
-    const DELIMITER_ALCDEF_KEY_VALUE = '=';
     const DELIMITER_ALCDEF_LINES = self::NEW_LINE_UNIX;
 
     /**
@@ -25,9 +24,19 @@ class AlcdefItem
     const FIELD_OBJECT_NAME = 'OBJECTNAME';
 
     /**
-     * Delimiter ending the metadata.
+     * Constants to split an ALCDEF unit into data and metadata.
      */
     const PATTERN_ALCDEF_METADATA_DATA = '@STARTMETADATA\\n(.*)\\nENDMETADATA\\n(.*)\\n@ms';
+    const SUBMATCH_INDEX_METADATA = 1;
+    const SUBMATCH_INDEX_DATA = 2;
+
+    /**
+     * Constants to fetch key and value from the ACLDEF line.
+     */
+    const DELIMITER_ALCDEF_KEY_VALUE = '=';
+    const PART_COUNT_ALCDEF_LINE = 2;
+    const ALCDEF_LINE_PART_INDEX_KEY = 0;
+    const ALCDEF_LINE_PART_INDEX_VALUE = 1;
 
     /**
      * @var mixed[]
@@ -39,22 +48,18 @@ class AlcdefItem
      */
     public function __construct($alcdefString)
     {
-        $this->alcdefArray = static::parseAlcdefArray($alcdefString);
+        $this->loadAlcdefArray($alcdefString);
     }
 
     /**
      * @param string $alcdefString
-     *
-     * @return mixed[]
      */
-    private static function parseAlcdefArray($alcdefString)
+    private function loadAlcdefArray($alcdefString)
     {
-        $alcdefString = static::cleanString($alcdefString);
+        $alcdefString = $this->normalizeNewlines($alcdefString);
         preg_match(self::PATTERN_ALCDEF_METADATA_DATA, $alcdefString, $alcdefParts);
-        $alcdefMetadata = static::parseAlcdefMetadata($alcdefParts[1]);
-        $alcdefData = static::parseAlcdefData($alcdefParts[2]);
-
-        return array_merge($alcdefMetadata, [self::FIELD_DATA => $alcdefData]);
+        $this->loadAlcdefMetadata($alcdefParts[self::SUBMATCH_INDEX_METADATA]);
+        $this->loadAlcdefData($alcdefParts[self::SUBMATCH_INDEX_DATA]);
     }
 
     /**
@@ -62,43 +67,33 @@ class AlcdefItem
      *
      * @return string
      */
-    private static function cleanString($string)
+    private function normalizeNewlines($string)
     {
         return preg_replace(self::PATTERN_NEW_LINE_ANY, self::NEW_LINE_UNIX, $string);
     }
 
     /**
      * @param string $alcdefMetadataString
-     *
-     * @return mixed[]
      */
-    private static function parseAlcdefMetadata($alcdefMetadataString)
+    private function loadAlcdefMetadata($alcdefMetadataString)
     {
-        $metadataFields = [];
-
         foreach (explode(self::DELIMITER_ALCDEF_LINES, $alcdefMetadataString) as $line) {
-            $field = explode(self::DELIMITER_ALCDEF_KEY_VALUE, $line, 2);
-            $metadataFields[$field[0]] = $field[1];
+            $field = explode(self::DELIMITER_ALCDEF_KEY_VALUE, $line, self::PART_COUNT_ALCDEF_LINE);
+            $this->alcdefArray[$field[self::ALCDEF_LINE_PART_INDEX_KEY]] = $field[self::ALCDEF_LINE_PART_INDEX_VALUE];
         }
-
-        return $metadataFields;
     }
 
     /**
      * @param string $alcdefDataString
-     *
-     * @return mixed[]
      */
-    private static function parseAlcdefData($alcdefDataString)
+    private function loadAlcdefData($alcdefDataString)
     {
-        $data = [];
+        $this->alcdefArray[self::FIELD_DATA] = [];
 
         foreach (explode(self::DELIMITER_ALCDEF_LINES, $alcdefDataString) as $line) {
-            $field = explode(self::DELIMITER_ALCDEF_KEY_VALUE, $line, 2);
-            $data[] = $field[1];
+            $field = explode(self::DELIMITER_ALCDEF_KEY_VALUE, $line, self::SUBMATCH_INDEX_DATA);
+            $this->alcdefArray[self::FIELD_DATA][] = $field[self::SUBMATCH_INDEX_METADATA];
         }
-
-        return $data;
     }
 
     /**
@@ -106,6 +101,20 @@ class AlcdefItem
      */
     public function getObjectName()
     {
-        return $this->alcdefArray[self::FIELD_OBJECT_NAME];
+        return $this->getFieldByName(self::FIELD_OBJECT_NAME);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return mixed|null
+     */
+    private function getFieldByName($name)
+    {
+        if (isset($this->alcdefArray[$name])) {
+            return $this->alcdefArray[$name];
+        } else {
+            return null;
+        }
     }
 }
